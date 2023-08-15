4ec73970b18204024e0afd755ba561b62dc3da0f
Valleybee <csplus2010.vb@gmail.com>
2023-08-15 11:27:19 +0000

    update config

--- config/botsmanagerconf.php
+++ config/botsmanagerconf.php
@@ -255,7 +255,7 @@ return [
             [
                 ['text' => 'PhD', 'callback_data' => 'k=1'],
                 ['text' => 'Engineer', 'callback_data' => 'k=2'],
-                ['text' => 'PastorJhon ', 'callback_data' => 'k=3'],
+                ['text' => 'PastorJohnJohn ', 'callback_data' => 'k=3'],
             ],
 
 
