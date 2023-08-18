bebcb1dc49bc23b4d668ac15f6360a1a56b0eb68
Valleybee <csplus2010.vb@gmail.com>
2023-08-18 10:42:29 +0000

    update methods

--- app/Http/Helpers/ResponseCallBackQueryMessages.php
+++ app/Http/Helpers/ResponseCallBackQueryMessages.php
@@ -40,7 +40,6 @@ class ResponseCallBackQueryMessages
         $this->first_name = $first_name;
         $this->last_name = $last_name;
         $this->content = $content;
-
     }
     public static function ResponseCallBackQueryMessages(Collection $responseFromTmbot)
     {
